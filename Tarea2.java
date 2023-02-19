b8000e06cd5fc0a5579a0797217593abf0d3d1fc
Adric208 <124096341+Adric208@users.noreply.github.com>
2023-02-19 16:09:50 -0600

    Update Tarea2.java

--- Tarea2.java
+++ Tarea2.java
@@ -1,20 +1,16 @@
 import java.util.Scanner;
-public class Tarea2
-{
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-        System.out.println("Dame la cadena en el siguiente orden: 'Nombre Profesión Nacionalidad':");
+public class Tarea2{
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+        	System.out.println("Dame la cadena en el siguiente orden: 'Nombre Profesión Pais de Origen':");
         String x = sc.nextLine();
-
         String[] p = x.split(" ");
         String a = p[0].toUpperCase();
         String b = p[1].toUpperCase();
         String c = p[2].toUpperCase();
-
-        System.out.println("Nombre: " + a);
-        System.out.println("Profesión: " + b);
-        System.out.println("País: " + c);
+        	System.out.println("Nombre: " + a);
+        	System.out.println("Profesión: " + b);
+       		System.out.println("País: " + c);
     }
 }
 	
