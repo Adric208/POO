e992d8a6a348620e650e20af74143a7b08e86ff7
Adric208 <124096341+Adric208@users.noreply.github.com>
2023-02-19 16:02:42 -0600

    Add files via upload

---
+++ Tarea2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Tarea2
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+        System.out.println("Dame la cadena en el siguiente orden: 'Nombre Profesión Nacionalidad':");
+        String x = sc.nextLine();
+
+        String[] p = x.split(" ");
+        String a = p[0].toUpperCase();
+        String b = p[1].toUpperCase();
+        String c = p[2].toUpperCase();
+
+        System.out.println("Nombre: " + a);
+        System.out.println("Profesión: " + b);
+        System.out.println("País: " + c);
+    }
+}
+	
+
