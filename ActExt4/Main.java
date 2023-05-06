9a74eed820932533388b80174b32dde8d7c96d0d
Adric208 <124096341+Adric208@users.noreply.github.com>
2023-05-06 12:20:49 -0600

    Add files via upload

---
+++ ActExt4/Main.java
@@ -0,0 +1,10 @@
+package ActExt4;
+
+import Tarea13.VentanaSwing;
+
+public class Main {
+    public static void main(String[] args) {
+        Ventana ventana = new Ventana("Paneles");
+
+    }
+}
---
+++ Tarea13/Main.java
@@ -0,0 +1,7 @@
+package Tarea13;
+
+public class Main {
+    public static void main(String[] args) {
+        VentanaSwing ventanaSwing = new VentanaSwing("Centigrados a farenheid");
+    }
+}
